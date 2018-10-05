907b28c8e20d4066b9e27719edc4a225e2c4b6fb
swve <bdrswv@gmail.com>
2018-10-05 11:32:13 +0200

    Security config

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -106,7 +106,13 @@ class User
 
     public function getRoles(): ?array
     {
-        return $this->roles;
+        $roles =  $this->roles;
+         // Afin d'être sûr qu'un user a toujours au moins 1 rôle
+         if (empty($roles)) {
+            $roles[] = 'ROLE_USER';
+        }
+ 
+        return array_unique($roles);
     }
 
     public function setRoles(?array $roles): self
@@ -139,4 +145,47 @@ class User
 
         return $this;
     }
+
+    /////////////////////
+    // Tutorial code : 
+    /////////////////////
+
+
+    public function getSalt(): ?string
+    {
+        // See "Do you need to use a Salt?" at https://symfony.com/doc/current/cookbook/security/entity_provider.html
+        // we're using bcrypt in security.yml to encode the password, so
+        // the salt value is built-in and you don't have to generate one
+ 
+        return null;
+    }
+
+      /**
+     * Removes sensitive data from the user.
+     *
+     * {@inheritdoc}
+     */
+    public function eraseCredentials(): void
+    {
+        // Nous n'avons pas besoin de cette methode car nous n'utilions pas de plainPassword
+        // Mais elle est obligatoire car comprise dans l'interface UserInterface
+        // $this->plainPassword = null;
+    }
+
+     /**
+     * {@inheritdoc}
+     */
+    public function serialize(): string
+    {
+        return serialize([$this->id, $this->username, $this->password]);
+    }
+
+     /**
+     * {@inheritdoc}
+     */
+    public function unserialize($serialized): void
+    {
+        [$this->id, $this->username, $this->password] = unserialize($serialized, ['allowed_classes' => false]);
+    }
+    
 }
