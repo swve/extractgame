e2a2ffe43772e8cd9d42d0ddc010f89ccbec6794
swve <bdrswv@gmail.com>
2018-10-06 20:29:55 +0200

    Fixing Security

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -1,13 +1,13 @@
 <?php
 
 namespace App\Entity;
-
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
  */
-class User
+class User implements UserInterface
 {
     /**
      * @ORM\Id()
