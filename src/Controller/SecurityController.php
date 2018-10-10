3227b450b8dead40ebd93fa761f1ab2c47da43c9
swve <bdrswv@gmail.com>
2018-10-10 23:02:46 +0200

    Reset password feature add

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -4,12 +4,12 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\RegistrationType;
-use Symfony\Component\HttpFoundation\Request;
 use Doctrine\Common\Persistence\ObjectManager;
-use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class SecurityController extends AbstractController
 {
@@ -23,7 +23,7 @@ class SecurityController extends AbstractController
         ]);
     }
 
-     /**
+    /**
      * @Route("/login", name="security_login")
      */
     public function login(AuthorizationCheckerInterface $authChecker)
@@ -39,10 +39,10 @@ class SecurityController extends AbstractController
         ]);
     }
 
-     /**
+    /**
      * @Route("/register", name="security_register")
      */
-    public function register(Request $request , ObjectManager $manager , UserPasswordEncoderInterface $encoder , AuthorizationCheckerInterface $authChecker , \Swift_Mailer $mailer)
+    public function register(Request $request, ObjectManager $manager, UserPasswordEncoderInterface $encoder, AuthorizationCheckerInterface $authChecker, \Swift_Mailer $mailer)
     {
 
         // Redirection if the user is already registered and logged in
@@ -53,38 +53,34 @@ class SecurityController extends AbstractController
         $user = new User();
         $form = $this->createForm(RegistrationType::class, $user);
 
-
         // 2) handle the submit (will only happen on POST)
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
 
             $user->setAvatarImg('nothing');
             $encoded = $encoder->encodePassword($user, $user->getPassword());
-            $plain_password= $user->getPassword() ; 
+            $plain_password = $user->getPassword();
             $user->setPassword($encoded);
             $mail_user = $user->getEmail();
             $username_user = $user->getUsername();
             $manager->persist($user);
             $manager->flush();
 
-
-
-            // Sending an Email 
+            // Sending an Email
 
             $message = (new \Swift_Message('Extract | Inscription réussie '))
-            ->setFrom('extract.mission312@gmail.com')
-            ->setTo($mail_user)
-            ->setBody(
-                $this->renderView(
-                    // templates/emails/registration.html.twig
-                    'emails/registration.html.twig',
-                    array('username' => $username_user , 'plain_pass' => $plain_password )
-                ),
-                'text/html'
-            );
-    
-            $mailer->send($message);
+                ->setFrom('extract.mission312@gmail.com')
+                ->setTo($mail_user)
+                ->setBody(
+                    $this->renderView(
+                        // templates/emails/registration.html.twig
+                        'emails/registration.html.twig',
+                        array('username' => $username_user, 'plain_pass' => $plain_password)
+                    ),
+                    'text/html'
+                );
 
+            $mailer->send($message);
 
             // maybe set a "flash" success message for the user
 
@@ -95,34 +91,83 @@ class SecurityController extends AbstractController
         ]);
     }
 
-     /**
+    /**
      * @Route("/logout", name="security_logout")
      */
     public function logout()
     {
-        
+
     }
 
-      /**
+    /**
      * @Route("/reset", name="security_reset")
      */
-    public function reset(Request $request)
+    public function reset(Request $request, UserPasswordEncoderInterface $encoder, \Swift_Mailer $mailer)
     {
-      
+
         $id = $request->get('user_id');
         $email = $request->get('user_email');
+
        
-       // $request->query->get('user_id');
 
-      
-        $this->addFlash('info', "formulaire non disponible");
-      
+        if ( $request->isMethod('POST') ) {
+            
+            $entityManager = $this->getDoctrine()->getManager();
+            $user = $entityManager->getRepository(User::class)->findOneBy(['email' => $email]);
 
+            if ($user) {
+              
+                            $username_user = $user->getUsername();
+                            
+
+                                // Generate password
+                                function generateRandomString($length = 10)
+                                {
+                                    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+                                    $charactersLength = strlen($characters);
+                                    $randomString = '';
+                                    for ($i = 0; $i < $length; $i++) {
+                                        $randomString .= $characters[rand(0, $charactersLength - 1)];
+                                    }
+                                    return $randomString;
+                                }
+                                $plain_password = generateRandomString();
+
+                                $encoded = $encoder->encodePassword($user, $plain_password);
+                                $user->setPassword($encoded);
+                                $entityManager->flush();
+
+                                // Sending an Email
+
+                                $message = (new \Swift_Message('Extract | Mot de passe changé  '))
+                                    ->setFrom('extract.mission312@gmail.com')
+                                    ->setTo($email)
+                                    ->setBody(
+                                        $this->renderView(
+                                            // templates/emails/registration.html.twig
+                                            'emails/reset.html.twig',
+                                            array('username' => $username_user, 'plain_pass' => $plain_password)
+                                        ),
+                                        'text/html'
+                                    );
+
+                                $mailer->send($message);
+
+                                $this->addFlash('success', "Mot de passe changé avec succès , veuillez verifier vos emails ");
+                        }
+            elseif (!$user) {
+                $this->addFlash('fail', "Aucun utilisateur avec cette adresse mail ");
+                $user = "null";
+            }
+           
+
+        }
+           
+        
+      
         return $this->render('security/reset.html.twig', [
-            'user_id' => $id , 'user_email' => $email
+            'user_id' => $id, 'user_email' => $email,
         ]);
 
-       
-        
     }
 }
