1dac631dda1855f39be105ed6ec2acd2bc02a002
swve <bdrswv@gmail.com>
2018-10-18 15:46:06 +0200

    Login fix

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Controller;
-
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -10,8 +10,12 @@ class DefaultController extends AbstractController
     /**
      * @Route("/", name="index_route")
      */
-    public function index()
+    public function index(AuthorizationCheckerInterface $authChecker)
     {
+           // Redirection if the user is already registered and logged in
+        if (true === $authChecker->isGranted('IS_AUTHENTICATED_FULLY')) {
+            return $this->redirectToRoute('home_route');
+        }
         return $this->render('default/index.html.twig');
     }
 
